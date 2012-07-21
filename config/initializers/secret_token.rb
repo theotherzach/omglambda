81a6b8f514c66c8a04fecce8bfb804e4db66867c
Zach Briggs <briggszj@gmail.com>
2012-07-21 16:35:34 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omglambda::Application.config.secret_token = '857b4ada4580aa9d119c07c76869a358196ce7dafa8a83948de1a1cab986e64ea1c6bfff021b3824035825bceddc7e2829ffaf839e17c8f35807f7f8da1d3f85'
